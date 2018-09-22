54e0a9d3992c16edc95393bfb623c7b6efb64c32
Valentinsan0107 <43492258+Valentinsan0107@users.noreply.github.com>
2018-09-22 23:19:28 +0200

    Début-zone-apercu-livre

--- livre.php
+++ livre.php
@@ -2,6 +2,52 @@
   include_once("includes/modules/header.php");
 ?>
 
+<div class="liste-présentation-affiliation-livre-conteneur">
+	<div class="présentation-livre-affiliation">
+		<div class="photo-livre-et-auteur">
+			
+		</div>
+
+		<div class="titre-livre-auteur-description-conteneur">
+			<div class="titre-livre">
+				
+			</div>
+			<div class="nom-auteur">
+				
+			</div>
+			<div class="description-livre-aperçu">
+				
+			</div>
+		</div>
+
+			<div class="lien-affiliation-amazon">
+				
+			</div>
+	</div>
+
+	<div class="présentation-livre-affiliation">
+		<div class="photo-livre-et-auteur">
+			
+		</div>
+
+		<div class="titre-livre-auteur-description-conteneur">
+			<div class="titre-livre">
+				
+			</div>
+			<div class="nom-auteur">
+				
+			</div>
+			<div class="description-livre-aperçu">
+				
+			</div>
+		</div>
+
+			<div class="lien-affiliation-amazon">
+				
+			</div>
+	</div>
+</div>
+
 
 
 <?php
