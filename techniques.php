92b99498f0725864b92368bcb328de8ac6bd4b5d
Valentinsan0107 <43492258+Valentinsan0107@users.noreply.github.com>
2018-09-22 11:58:54 +0200

    J'ai retiré article bite

--- techniques.php
+++ techniques.php
@@ -83,22 +83,6 @@
 
 </div>
 
-<div class="article-conteneur">
-	<div class="zone-article">
-		<h1 class="titre-article">Je suis le titre de l'aperçu de l'article</h1>
-		<p class="texte-article">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur […]</p>
-		<a class="lien-article" href="articles/article-bite.php">Lire plus...</a>
-	<div class="conteneur-module-comment">
-		<a class="lien-icon-conversation" href="articles/article-bite.php">
-			<img class="icon-conversation" src="img/conversation.png">
-		</a>
-<?php
-	$nomfileshow =  "psychoschoolVal/articles/article-bite.php";
-    include("includes/modules/shownbcoment.php");
-?>
-	</div>
-</div>
-
 
 
 <?php
