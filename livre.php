eb61acd156936b193f5470cc94251c336c6a799d
Valentinsan0107 <43492258+Valentinsan0107@users.noreply.github.com>
2018-09-23 19:44:06 +0200

    Avancé livre
    Progression du css html de la partie livre c'est encore en mode test

--- livre.php
+++ livre.php
@@ -9,14 +9,17 @@
 		</div>
 
 		<div class="titre-livre-auteur-description-conteneur">
-			<div class="titre-livre">
-				
+			<div class="titre-livre-conteneur">
+				<h2 class="titre-livre">Titre livre 1</h2>
 			</div>
-			<div class="nom-auteur">
-				
+			<div class="nom-auteur-conteneur">
+				<h3 class="nom-auteur">Auteur de ce livre : <a class="lien-nom-auteur" href="livres/livre-1.php">Nom de l'auteur</a></h3>
 			</div>
-			<div class="description-livre-aperçu">
-				
+			<div class="description-livre-aperçu-conteneur">
+				<h3 class="titre-paragraphe-description-livre">Titre description livre 1</h3>
+				<p class="paragraphe-description-livre">Coactique aliquotiens nostri pedites ad eos persequendos scandere clivos sublimes etiam si lapsantibus plantis fruticeta prensando vel dumos ad vertices venerint summos, inter arta tamen et invia nullas acies explicare permissi nec firmare nisu valido gressus: hoste discursatore rupium abscisa volvente, ruinis ponderum inmanium consternuntur, aut ex necessitate ultima fortiter dimicante, superati periculose per prona discedunt. aut ex necessitate ultima fortiter dimicante, superati periculose per prona discedunt.</p>
+
+				<a class="lien-article" href="livres/livre-1.php">En savoir plus...</a>
 			</div>
 		</div>
 
@@ -25,27 +28,6 @@
 			</div>
 	</div>
 
-	<div class="présentation-livre-affiliation">
-		<div class="photo-livre-et-auteur">
-			
-		</div>
-
-		<div class="titre-livre-auteur-description-conteneur">
-			<div class="titre-livre">
-				
-			</div>
-			<div class="nom-auteur">
-				
-			</div>
-			<div class="description-livre-aperçu">
-				
-			</div>
-		</div>
-
-			<div class="lien-affiliation-amazon">
-				
-			</div>
-	</div>
 </div>
 
 
