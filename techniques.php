4cf7a99a222d8dcebc8088ebaea1ead948c9b0e7
Victorien <victorienmarette.pro@gmail.com>
2018-10-06 19:51:16 +0200

    Nouveau system plus simple technique

--- techniques.php
+++ techniques.php
@@ -5,44 +5,69 @@
 
 <div class="article-conteneur">
 <?php
-    $sql = "SELECT * FROM techniqueArticle";
+    $sql = "SELECT * FROM article WHERE article_thechnique='1' AND article_priorite='1'";
 	$result = mysqli_query($conn, $sql);
 	$resultChek = mysqli_num_rows($result);
 
 	if ($resultChek > 0) {
 		while ($row = mysqli_fetch_assoc($result)) {
-			$nompageserv = $row['Techarticle_page'];
-			$sql = "SELECT * FROM article WHERE article_page='$nompageserv'";
-			$result2 = mysqli_query($conn, $sql);
-			$resultChek = mysqli_num_rows($result2);
-			if ($resultChek === 1) {
-				while ($row2 = mysqli_fetch_assoc($result2)) {
-					echo '<div class="zone-article">
-						<h1 class="titre-article">'.$row2['article_title'].'</h1>
-						<p class="texte-article">'.$row2['article_contenue'].'</p>
-						<a class="lien-article" href="/'.$nompageserv.'">Lire plus...</a>
-					<div class="conteneur-module-comment">
-						<a class="lien-icon-conversation" href="/'.$nompageserv.'">
-							<img class="icon-conversation" src="img/conversation.png">
-						</a>';
+			$nompageserv = $row['article_page'];
+			echo '<div class="zone-article">
+				<h1 class="titre-article">'.$row['article_title'].'</h1>
+				<p class="texte-article">'.$row['article_contenue'].'</p>
+				<a class="lien-article" href="/'.$nompageserv.'">Lire plus...</a>
+			<div class="conteneur-module-comment">
+				<a class="lien-icon-conversation" href="/'.$nompageserv.'">
+					<img class="icon-conversation" src="img/conversation.png">
+				</a>';
 
-					$sql = "SELECT * FROM comentaires WHERE coment_page='$nompageserv'";
-					$result3 = mysqli_query($conn, $sql);
-					$resultChek = mysqli_num_rows($result3);
+			$sql = "SELECT * FROM comentaires WHERE coment_page='$nompageserv'";
+			$result3 = mysqli_query($conn, $sql);
+			$resultChek = mysqli_num_rows($result3);
 
-					echo '<p class="nombre-commentaire-article">'.$resultChek.'</p>';
+			echo '<p class="nombre-commentaire-article">'.$resultChek.'</p>';
 
-					$sql = "SELECT * FROM likes WHERE like_page='$nompageserv'";
-					$result3 = mysqli_query($conn, $sql);
-					$resultChek = mysqli_num_rows($result3);
+			$sql = "SELECT * FROM likes WHERE like_page='$nompageserv'";
+			$result3 = mysqli_query($conn, $sql);
+			$resultChek = mysqli_num_rows($result3);
 
-					echo '<p>'.$resultChek.'</p>';
+			echo '<p>'.$resultChek.'</p>';
 
-					echo '</div>
-					</div>';
+			echo '</div>
+			</div>';
+		}
+	}
 
-				}
-			}
+	$sql = "SELECT * FROM article WHERE article_thechnique='1' AND article_priorite='0'";
+	$result = mysqli_query($conn, $sql);
+	$resultChek = mysqli_num_rows($result);
+
+	if ($resultChek > 0) {
+		while ($row2 = mysqli_fetch_assoc($result)) {
+			$nompageserv = $row2['article_page'];
+			echo '<div class="zone-article">
+				<h1 class="titre-article">'.$row2['article_title'].'</h1>
+				<p class="texte-article">'.$row2['article_contenue'].'</p>
+				<a class="lien-article" href="/'.$nompageserv.'">Lire plus...</a>
+			<div class="conteneur-module-comment">
+				<a class="lien-icon-conversation" href="/'.$nompageserv.'">
+					<img class="icon-conversation" src="img/conversation.png">
+				</a>';
+
+			$sql = "SELECT * FROM comentaires WHERE coment_page='$nompageserv'";
+			$result3 = mysqli_query($conn, $sql);
+			$resultChek = mysqli_num_rows($result3);
+
+			echo '<p class="nombre-commentaire-article">'.$resultChek.'</p>';
+
+			$sql = "SELECT * FROM likes WHERE like_page='$nompageserv'";
+			$result3 = mysqli_query($conn, $sql);
+			$resultChek = mysqli_num_rows($result3);
+
+			echo '<p>'.$resultChek.'</p>';
+
+			echo '</div>
+			</div>';
 		}
 	}
 ?>
