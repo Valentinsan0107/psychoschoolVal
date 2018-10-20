57421940e932c7a832411825f831acb23929b66c
Valentinsan0107 <43492258+Valentinsan0107@users.noreply.github.com>
2018-10-20 22:48:28 +0200

    modif

--- article.php
+++ article.php
@@ -60,7 +60,7 @@ if (isset($_GET['narticle'])) {
 		$result = mysqli_query($conn, $sql);
 		$resultChek = mysqli_num_rows($result);
 
-		echo '<p>'.$resultChek.'<p>';
+		echo '<p class="nombre-like-article">'.$resultChek.'<p>';
 
 		$userlikechek = 0;
 
