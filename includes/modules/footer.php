c324b59e6b3f224e1a7d4b36e7092b863e951f3b
Victorien <victorienmarette.pro@gmail.com>
2018-10-21 13:04:18 +0200

    modif footer

--- includes/modules/footer.php
+++ includes/modules/footer.php
@@ -6,10 +6,23 @@
     <h1 class="titre-section-footer"></h1>
 
     <ul>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
+      <?php
+        $pref = __FILE__;
+        $pos = strpos($pref, "psychoschoolVal");
+        $longeur = strlen($pref);
+        $aefface = $pos+15-$longeur;
+        $pref = substr($pref, 0, $aefface);
+        include_once($pref."/includes/backrownd/dph.php");
+
+        $sql = "SELECT * FROM article";
+        $result = mysqli_query($conn, $sql);
+        $resultChek = mysqli_num_rows($result);
+        if ($resultChek >0) {
+          while ($row = mysqli_fetch_assoc($result)) {
+            echo "<li>".$row['article_titre']."</li>";
+          }
+        }
+      ?>
     </ul>
     
   </div>
