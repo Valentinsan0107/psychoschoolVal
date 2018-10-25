cd9c7a04ccbedbb5ab4e137efadb7dbe5e1000c4
Victorien <victorienmarette.pro@gmail.com>
2018-10-25 19:36:04 +0200

    re

--- recherche.php
+++ recherche.php
@@ -2,13 +2,14 @@
   include_once("includes/modules/header.php");
 
   if (isset($_GET['submit'])) {
+    echo "<div class='article-conteneur'>";
   	include_once('includes/backrownd/dph.php');
   	$recherche = mysqli_real_escape_string($conn, $_GET['recherche']);
   	$recherches = explode(" ", $recherche);
 
 
   	foreach ($recherches as $r) {
-  		if (strlen($r) > 1) {
+  		if (strlen($r) > 2) {
   			$sql = $sql = "SELECT * FROM article WHERE article_titre LIKE '%$r%' OR article_resumer LIKE '%$r%' OR article_tag LIKE '%$r%'";
   			$result = mysqli_query($conn, $sql);
   			$resultChek = mysqli_num_rows($result);
@@ -23,5 +24,6 @@
   		}
   	}  	
   }
+  echo "</div>";
   include_once("includes/modules/footer.php");
 ?>
