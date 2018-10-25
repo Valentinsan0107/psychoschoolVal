70958a3532963a01b608dfe8ddc2eecd6eede28e
Victorien <victorienmarette.pro@gmail.com>
2018-10-25 21:36:37 +0200

    final

--- recherche.php
+++ recherche.php
@@ -3,8 +3,26 @@
 
   if (isset($_GET['submit'])) {
     echo "<div class='article-conteneur'>";
-  	include_once('includes/backrownd/dph.php');
-  	$recherche = mysqli_real_escape_string($conn, $_GET['recherche']);
+    include_once('includes/backrownd/dph.php');
+    $recherche = mysqli_real_escape_string($conn, $_GET['recherche']);
+    $artselec = array();
+
+    $sql = $sql = "SELECT * FROM article WHERE article_titre LIKE '%$recherche%' OR article_resumer LIKE '%$recherche%' OR article_tag LIKE '%$recherche%'";
+    $result = mysqli_query($conn, $sql);
+    $resultChek = mysqli_num_rows($result);
+
+    if ($resultChek > 0) {
+      while ($row = mysqli_fetch_assoc($result)) {
+        if (!in_array($row['article_id'], $artselec)) {
+          array_push($artselec, $row['article_id']);
+          if ($row['article_thechnique'] == 1) {
+            include("includes/modules/pres/presart.php");
+          }
+        }
+      }
+    }
+
+
   	$recherches = explode(" ", $recherche);
 
 
@@ -16,9 +34,12 @@
 
   			if ($resultChek > 0) {
   				while ($row = mysqli_fetch_assoc($result)) {
-  					if ($row['article_thechnique'] == 1) {
-  						include("includes/modules/pres/presart.php");
-  					}
+            if (!in_array($row['article_id'], $artselec)) {
+              array_push($artselec, $row['article_id']);
+              if ($row['article_thechnique'] == 1) {
+                include("includes/modules/pres/presart.php");
+              }
+            }
   				}
   			}
   		}
