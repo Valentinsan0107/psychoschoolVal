0cdc9ee013baa10318c87e7245207953f13d244b
Victorien <victorienmarette.pro@gmail.com>
2018-11-02 13:17:48 +0100

    nv

--- autre/exampleArticle.php
+++ autre/exampleArticle.php
@@ -1,5 +1,6 @@
 <h1 class="titre-article">Je suis le titre de larticle en entier</h1>
-	
+
+
 
 <p class="paragraphe-article-complet">Novo denique perniciosoque exemplo idem Gallus ausus est inire flagitium grave, quod Romae cum ultimo dedecore temptasse aliquando dicitur Gallienus, et adhibitis paucis clam ferro succinctis vesperi per tabernas palabatur et conpita quaeritando Graeco sermone, cuius erat inpendio gnarus, quid de Caesare quisque sentiret. et haec confidenter agebat in urbe ubi pernoctantium luminum claritudo dierum solet imitari fulgorem. postremo agnitus saepe iamque, si prodisset, conspicuum se fore contemplans, non nisi luce palam egrediens ad agenda quae putabat seria cernebatur. et haec quidem medullitus multis gementibus agebantur.</p>
 
