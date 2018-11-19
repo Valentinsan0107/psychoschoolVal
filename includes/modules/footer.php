6e7b0678a892aecc9c6afbd475cd6c2244e6012a
Victorien <victorienmarette.pro@gmail.com>
2018-11-19 22:17:51 +0100

    pute

--- includes/modules/footer.php
+++ includes/modules/footer.php
@@ -78,48 +78,6 @@
 
 </div>
 
-<div class="conteneur-contenu-footer-bas">
-  <p class="texte-copyright">© Psychoschool - All right reserved.</p>
-  <form class="barre-recherche-conteneur" method="GET" action="recherche.php">
-    <input id="champ-recherche" type="text" name="recherche" minlength="1" placeholder="Tu cherches quelque chose ?">
-    <button id="search-button" type="submit" name="submit"><img class="image-barre-recherche" src="img/loupe-barre-recherche.png"></button>
-  </form>
-    
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   
     <script>
 $(document).ready(function() {
